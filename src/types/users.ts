bbe38c3bc55825ae764064dc1edf213d9d1fc577
andreseichi <andre_seichi@hotmail.com>
2022-09-15 03:35:14 -0300

    user

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,7 +1,12 @@
 export interface User {
-  id: string;
+  id: number;
   email: string;
   password: string;
+  confirmPassword: string;
 }
 
-export type UserInsertData = Omit<User, "id">;
+export type UserToken = Omit<User, "password" | "confirmPassword">;
+
+export type UserData = Omit<User, "id">;
+
+export type UserInsertData = Omit<User, "id" | "confirmPassword">;
