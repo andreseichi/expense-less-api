10069ff883718c35bcbcd3c70fdf0b9b770b1e9d
andreseichi <andre_seichi@hotmail.com>
2022-09-15 03:39:00 -0300

    auth schemas with confirm password

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,6 +1,14 @@
 import joi from "joi";
 
-export const signUpSchema = joi.object({
+export const signUpSchema = joi
+  .object({
+    email: joi.string().email().trim().required(),
+    password: joi.string().min(10).required(),
+    confirmPassword: joi.ref("password"),
+  })
+  .with("password", "confirmPassword");
+
+export const signInSchema = joi.object({
   email: joi.string().email().trim().required(),
   password: joi.string().min(10).required(),
 });
