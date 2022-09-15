f318b8ef3e366c8a9da4577839ec93f366ec218c
andreseichi <andre_seichi@hotmail.com>
2022-09-15 01:26:30 -0300

    main route /

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 
 import { authRouter } from "./auth.routes";
 
 const router = Router();
+
+router.get("/", (request: Request, response: Response) => {
+  return response.json({ message: "Hello World" });
+});
 
 router.use(authRouter);
 
