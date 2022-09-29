29850de76d9b43870f89cd253ce534ce976e3c25
andreseichi <andre_seichi@hotmail.com>
2022-09-29 20:49:39 -0300

    feat: middleware for token validation

---
+++ src/middlewares/tokenMiddleware.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+export const isAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const JWT_ACCESS_SECRET = String(process.env.JWT_ACCESS_SECRET);
+
+    const token: string = res.locals.headers.authorization.split(" ")[1];
+    const payload = jwt.verify(token, JWT_ACCESS_SECRET);
+
+    res.locals.payload = payload;
+  } catch (error: any) {
+    if (error.message === "jwt expired") {
+      return res.status(401).send({ message: "Token expired" });
+    }
+    return res.status(401).send({ message: "Invalid token" });
+  }
+
+  return next();
+};
