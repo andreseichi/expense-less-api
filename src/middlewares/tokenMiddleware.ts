5e0ebc6603bf7b3aca8ca353db62b5641c0a723b
andreseichi <andre_seichi@hotmail.com>
2022-10-01 00:37:35 -0300

    fix: json parse token

--- src/middlewares/tokenMiddleware.ts
+++ src/middlewares/tokenMiddleware.ts
@@ -13,7 +13,7 @@ export const isAuthenticated = (
     const JWT_ACCESS_SECRET = String(process.env.JWT_ACCESS_SECRET);
 
     const token: string = res.locals.headers.authorization.split(" ")[1];
-    const payload = jwt.verify(token, JWT_ACCESS_SECRET);
+    const payload = jwt.verify(JSON.parse(token), JWT_ACCESS_SECRET);
 
     res.locals.payload = payload;
   } catch (error: any) {
