8acb80a150eed7affa6268c08b6b6f34888fe8d0
andreseichi <andre_seichi@hotmail.com>
2022-10-10 09:32:10 -0300

    fix: removed dist

--- dist/src/types/transaction.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TransactionType;
-(function (TransactionType) {
-    TransactionType["income"] = "income";
-    TransactionType["expense"] = "expense";
-})(TransactionType || (TransactionType = {}));
