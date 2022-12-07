6cb0b5265abb7275179bfe17f5cb26d62ae32637
andreseichi <andre_seichi@hotmail.com>
2022-12-07 11:21:18 -0300

    password minimum length 5

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -5,12 +5,12 @@ export const signUpSchema = joi
     name: joi.string().trim().required(),
     email: joi.string().email().trim().required(),
     pictureUrl: joi.string().trim().uri().allow("", null).optional(),
-    password: joi.string().min(10).required(),
+    password: joi.string().min(5).required(),
     confirmPassword: joi.ref("password"),
   })
   .with("password", "confirmPassword");
 
 export const signInSchema = joi.object({
   email: joi.string().email().trim().required(),
-  password: joi.string().min(10).required(),
+  password: joi.string().required(),
 });
