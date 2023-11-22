52f5c87521be89f2744ca59ea67a2c0168029fec
tashiro <andre_seichi@hotmail.com>
2023-11-22 20:48:33 -0300

    better case

--- src/middlewares/errorHandlingMiddleware.ts
+++ src/middlewares/errorHandlingMiddleware.ts
@@ -5,22 +5,25 @@ export const errorHandlingMiddleware = (
   error: errors,
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
-  if (error.type === "BAD_REQUEST")
-    return res.status(400).send({ message: error.message });
+  switch (error.type) {
+    case "BAD_REQUEST":
+      return res.status(400).send({ message: error.message });
 
-  if (error.type === "UNAUTHORIZED")
-    return res.status(401).send({ message: error.message });
+    case "UNAUTHORIZED":
+      return res.status(401).send({ message: error.message });
 
-  if (error.type === "FORBIDDEN")
-    return res.status(403).send({ message: error.message });
+    case "FORBIDDEN":
+      return res.status(403).send({ message: error.message });
 
-  if (error.type === "NOT_FOUND")
-    return res.status(404).send({ message: error.message });
+    case "NOT_FOUND":
+      return res.status(404).send({ message: error.message });
 
-  if (error.type === "CONFLICT")
-    return res.status(409).send({ message: error.message });
+    case "CONFLICT":
+      return res.status(409).send({ message: error.message });
 
-  return res.sendStatus(500);
+    default:
+      return res.status(500).send({ message: error.message });
+  }
 };
